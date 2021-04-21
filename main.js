0e169dac7044cbb2d932c50a6e4bf88665e52b1e
FLAVIE <flavie@MacBook-Pro-de-FLAVIE.local>
2021-04-21 14:18:25 +0200

    JS_Exercice_P4_4

---
+++ main.js
@@ -0,0 +1,24 @@
+var réponse;
+var question = prompt('quelle est la première couleur du drapeau français?', réponse);
+switch (question) {
+   case (réponse = 'bleu'):
+      alert('Bien joué !');
+      break;
+   case (réponse = 'rouge'):
+      alert('mais non!');
+      break;
+   case (réponse = 'blanc'):
+      alert('non non non!');
+      break;
+   case (réponse = 'rose'):
+      alert('franchement?!!');
+      break;
+   default:
+      alert('tu te fiches de moi!!');
+
+}
+
+
+
+
+
